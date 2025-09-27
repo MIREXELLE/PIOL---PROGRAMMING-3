c24043b8d90a063e44fb84b1a3ee1c59c67e01a0
Mirexelle <m.mirexellevincentc.piol@gmail.com>
2025-09-27 14:18:40 +0800

    number6

---
+++ Activity_4/number6.java
@@ -0,0 +1,19 @@
+package Activity_4;
+
+import java.util.Scanner;
+
+public class number6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter a number to know if it's odd or even: ");
+        int num = in.nextInt();
+
+        if (num%2 == 0){
+            System.out.println(num + " is a EVEN number");
+        } else {
+            System.out.println(num + " is a ODD number");
+        }
+
+    }
+}
