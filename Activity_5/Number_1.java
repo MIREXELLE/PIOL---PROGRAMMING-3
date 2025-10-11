3b33bac2e9a6e88336dabd52c0608a2a8c8c4e72
Mirexelle <m.mirexellevincentc.piol@gmail.com>
2025-10-11 16:16:32 +0800

    Activity 5 - Number 1

---
+++ Activity_5/Number_1.java
@@ -0,0 +1,15 @@
+package Activity_5;
+
+// 1. Write a program to print numbers from 1 to 10.
+public class Number_1 {
+    public static void main(String[] args) {
+        // Print a header message to indicate what the program does
+        System.out.println("Printing numbers from 1 to 10:");
+
+        // Initialize loop counter i to 1, continue while i <= 10, increment i by 1 each time
+        for (int i = 1; i <= 10; i++) {
+            // Print the current value of i followed by a newline
+            System.out.println(i);
+        }
+    }
+}
